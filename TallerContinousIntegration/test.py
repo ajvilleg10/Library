1aa0798c32076d7ec3ec390ee23003b28d3275a0
ajvilleg10 <ajvilleg@espol.edu.ec>
2023-12-06 20:30:35 -0500

    Añadiendo libreria pytest

--- TallerContinousIntegration/test.py
+++ TallerContinousIntegration/test.py
@@ -1,31 +1,26 @@
-import unittest
+import pytest
 from library import Library
 from main import select_books_for_checkout, process_book_returns
 
-class TestLibraryMethods(unittest.TestCase):
+@pytest.fixture
+def library():
+    return Library()
 
-    def setUp(self):
-        self.library = Library()
+def test_checkout_books(library):
+    selections = [{'book_index': 1, 'quantity': 2}, {'book_index': 2, 'quantity': 1}]
+    total_late_fee = library.checkout_books(selections)
+    assert total_late_fee == 0  # Assuming no overdue books for this test
 
-    def test_checkout_books(self):
-        selections = [{'book_index': 1, 'quantity': 2}, {'book_index': 2, 'quantity': 1}]
-        total_late_fee = self.library.checkout_books(selections)
-        self.assertEqual(total_late_fee, 0)  # Assuming no overdue books for this test
+def test_process_book_returns(library):
+    # Assume books are checked out first
+    checkout_selections = [{'book_index': 1, 'quantity': 2}, {'book_index': 2, 'quantity': 1}]
+    library.checkout_books(checkout_selections)
 
-    def test_process_book_returns(self):
-        # Assume books are checked out first
-        checkout_selections = [{'book_index': 1, 'quantity': 2}, {'book_index': 2, 'quantity': 1}]
-        self.library.checkout_books(checkout_selections)
+    returned_books = [{'book_index': 1, 'quantity': 1}]
+    total_late_fee = library.return_books(returned_books)
+    assert total_late_fee == 0  # Assuming no overdue books for this test
 
-        returned_books = [{'book_index': 1, 'quantity': 1}]
-        total_late_fee = self.library.return_books(returned_books)
-        self.assertEqual(total_late_fee, 0)  # Assuming no overdue books for this test
-
-    def test_calculate_total_late_fees(self):
-        # Assuming no overdue books for this test
-        total_late_fee = self.library.calculate_total_late_fees()
-        self.assertEqual(total_late_fee, 0)
-
-#Ejecución del main
-if __name__ == '__main__':
-    unittest.main()
+def test_calculate_total_late_fees(library):
+    # Assuming no overdue books for this test
+    total_late_fee = library.calculate_total_late_fees()
+    assert total_late_fee == 0
